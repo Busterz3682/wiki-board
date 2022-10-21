be0f6e38bd0bd628aaeb976cd59ec87e78faf08e
You Taein <ytn1574@gmail.com>
2022-10-21 21:15:58 +0900

    -

--- src/main/java/com/side/wiki/document/service/DocumentService.java
+++ src/main/java/com/side/wiki/document/service/DocumentService.java
@@ -1,14 +1,17 @@
 package com.side.wiki.document.service;
 
+import com.side.wiki.document.domain.DocumentVO;
+
 public interface DocumentService {
 
 	
 	//구현해야 할 기능들(CRUD)
 	//문서 작성
-	
+	void insertDoc();
 	//문서 조회
-	
+	DocumentVO getDocList();
 	//문서 수정
-	
+	void updateDoc();
 	//문서 삭제
+	void deleteDoc();
 }
--- src/main/java/com/side/wiki/document/service/DocumentServiceImpl.java
+++ src/main/java/com/side/wiki/document/service/DocumentServiceImpl.java
@@ -2,7 +2,37 @@ package com.side.wiki.document.service;
 
 import org.springframework.stereotype.Service;
 
+import com.side.wiki.document.domain.DocumentVO;
+
 @Service
-public class DocumentServiceImpl {
+public class DocumentServiceImpl implements DocumentService {
+
+	//문서 작성
+	@Override
+	public void insertDoc() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//문서 조회
+	@Override
+	public DocumentVO getDocList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//문서 수정
+	@Override
+	public void updateDoc() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//문서 삭제
+	@Override
+	public void deleteDoc() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
